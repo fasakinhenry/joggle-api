9e898909a6d81a11cd70c1bf26cf9f8bc06353fb
Fasakin Henry <fasakinhenry@gmail.com>
2025-07-15 15:32:16 +0100

    install express rate limit and passport dependencies
    npm install passport passport-google-oauth20 passport-facebook passport-linkedin-oauth2 passport-github2 express-rate-limit

--- models/User.js
+++ models/User.js
@@ -6,51 +6,76 @@ const userSchema = new mongoose.Schema({
     required: true,
     unique: true,
     trim: true,
-    lowercase: true
+    lowercase: true,
   },
   password: {
     type: String,
-    required: true
   },
   username: {
     type: String,
     required: true,
-    trim: true
+    trim: true,
   },
   isVerified: {
     type: Boolean,
-    default: false
+    default: false,
   },
   verificationToken: {
-    type: String
+    type: String,
   },
+  resetPasswordToken: {
+    type: String,
+  },
+  resetPasswordExpires: {
+    type: Date,
+  },
+  refreshToken: {
+    type: String,
+  },
+  oauthProviders: [
+    {
+      provider: String,
+      providerId: String,
+      accessToken: String,
+      refreshToken: String,
+    },
+  ],
   createdAt: {
     type: Date,
-    default: Date.now
+    default: Date.now,
   },
-  // Fields for e-learning platform
-  courses: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Course'
-  }],
+  courses: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Course',
+    },
+  ],
   socialProfiles: {
     type: Map,
-    of: String
+    of: String,
   },
-  friends: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User'
-  }],
-  followers: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User'
-  }],
-  badges: [{
-    type: String
-  }],
-  languages: [{
-    type: String
-  }]
+  friends: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    },
+  ],
+  followers: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    },
+  ],
+  badges: [
+    {
+      type: String,
+    },
+  ],
+  languages: [
+    {
+      type: String,
+    },
+  ],
 });
 
 module.exports = mongoose.model('User', userSchema);
