8d37507fb82b41f01c7cdb080af9691bdcdfc85e
Fasakin Henry <fasakinhenry@gmail.com>
2025-07-15 15:46:05 +0100

    feat: add code for auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -9,7 +9,9 @@ const authMiddleware = async (req, res, next) => {
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    const user = await User.findById(decoded.userId).select('-password');
+    const user = await User.findById(decoded.userId).select(
+      '-password -refreshToken'
+    );
     if (!user) {
       return res.status(401).json({ error: 'User not found' });
     }
