4e5dead6d846ef6be3381f81d16be88890548fc0
Fasakin Henry <fasakinhenry@gmail.com>
2025-07-17 23:38:25 +0100

    feat: update the db config

--- config/db.js
+++ config/db.js
@@ -1,13 +1,17 @@
 const mongoose = require('mongoose');
+const logger = require('./logger');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
-    console.log('Connected to MongoDB');
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    logger.info('MongoDB connected successfully');
   } catch (error) {
-    console.error('MongoDB connection error:', error);
+    logger.error('MongoDB connection error:', error);
     process.exit(1);
   }
 };
 
-module.exports = { connectDB };
+module.exports = connectDB;
